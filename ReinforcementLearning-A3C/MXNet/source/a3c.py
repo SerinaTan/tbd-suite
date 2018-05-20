50592ca2bc892021c241196d8d3485b33914dca2
Anand J <anand.indukala@gmail.com>
2018-05-19 21:54:53 -0400

    A3C: New features and changes
    Add numba into requirements.gpu-cu80.txt
    Add samples/sec
    Reducing print statements
    Remove fps logging
    Use PongDeterministic instead of Breakout

--- ReinforcementLearning-A3C/MXNet/source/a3c.py
+++ ReinforcementLearning-A3C/MXNet/source/a3c.py
@@ -115,7 +115,7 @@ def train():
         kv = None
 
     # module
-    dataiter = rl_data.GymDataIter('Breakout-v0', args.batch_size, args.input_length, web_viz=True)
+    dataiter = rl_data.GymDataIter('PongDeterministic-v0', args.batch_size, args.input_length, web_viz=True)
     net = sym.get_symbol_atari(dataiter.act_dim)
     module = mx.mod.Module(net, data_names=[d[0] for d in dataiter.provide_data], label_names=('policy_label', 'value_label'), context=devs)
     module.bind(data_shapes=dataiter.provide_data,
@@ -150,6 +150,8 @@ def train():
     score = np.zeros((args.batch_size, 1))
     final_score = np.zeros((args.batch_size, 1))
     iteration = 0
+    tic = time.time()
+    samples = 0
     for epoch in range(args.num_epochs):
         if save_model_prefix:
             module.save_params('%s-%04d.params'%(save_model_prefix, epoch))
@@ -167,7 +169,6 @@ def train():
                 print("Done calling profile_stop().")
             # </EcoSys>
 
-            tic = time.time()
             # clear gradients
             for exe in module._exec_group.grad_arrays:
                 for g in exe:
@@ -176,6 +177,7 @@ def train():
             S, A, V, r, D = [], [], [], [], []
             for t in range(args.t_max + 1):
                 data = dataiter.data()
+                samples+= sum([len(s) for s in data[0]])
                 module.forward(mx.io.DataBatch(data=data, label=None), is_train=False)
                 act, _, val = module.get_outputs()
                 V.append(val.asnumpy())
@@ -204,8 +206,6 @@ def train():
                 out_acts_tile=np.tile(-np.log(out_acts + 1e-7),(1, dataiter.act_dim))
                 module.backward([mx.nd.array(out_acts_tile*adv), h])
 
-                print('pi', pi[0].asnumpy())
-                print('h', h[0].asnumpy())
                 err += (adv**2).mean()
                 score += r[i]
                 final_score *= (1-D[i])
@@ -214,9 +214,11 @@ def train():
                 T += D[i].sum()
 
             module.update()
-            logging.info('fps: %f err: %f score: %f final: %f T: %f'%(args.batch_size/(time.time()-tic), err/args.t_max, score.mean(), final_score.mean(), T))
-            print(score.squeeze())
-            print(final_score.squeeze())
+
+            if iteration%100 == 99:
+                logging.info('err: %f score: %f final: %f T: %f Throughput: %f K/sec'%(err/args.t_max, score.mean(), final_score.mean(), T, (samples*0.001)/(time.time()-tic)))
+                tic = time.time()
+                samples = 0
             iteration += 1
 
 def test():
